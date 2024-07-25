3f74a62ea3f44a2597227537fdf11e443fe792ea
Rithik2211 <rithikpranao22@gmail.com>
2024-07-25 16:25:55 +0530

    "Updated Home page to include a welcome message and added SignInButton to ConvexClientProvider"

--- next-messaging-app/app/page.tsx
+++ next-messaging-app/app/page.tsx
@@ -1,5 +1,10 @@
 import { UserButton } from '@clerk/nextjs'
 
 export default function Home() {
-  return <UserButton />
+  return (
+    <div>
+      <UserButton />
+      <h1>Welcome green</h1>
+    </div>
+  )
 }
--- next-messaging-app/provider/ConvexClientProvider.tsx
+++ next-messaging-app/provider/ConvexClientProvider.tsx
@@ -1,7 +1,7 @@
 'use client'
-import { ClerkProvider, useAuth } from '@clerk/nextjs'
+import { ClerkProvider, SignInButton, useAuth } from '@clerk/nextjs'
 import { ConvexProviderWithClerk } from "convex/react-clerk";
-import { Authenticated, AuthLoading, ConvexReactClient } from "convex/react";
+import { Authenticated, AuthLoading, ConvexReactClient, Unauthenticated } from "convex/react";
 import React from 'react'
 import LoadingLogo from '@/components/shared/LoadingLogo';
 
@@ -14,8 +14,11 @@ const convex = new ConvexReactClient(CONVEX_URL);
 const ConvexClientProvider = ({children} : Props) => {
   return <ClerkProvider>
     <ConvexProviderWithClerk useAuth={useAuth} client={convex}>
-      <Authenticated>{children}</Authenticated>
-      <AuthLoading><LoadingLogo /></AuthLoading>
+    <Unauthenticated>
+        <SignInButton />
+    </Unauthenticated>
+    <Authenticated>{children}</Authenticated>
+    <AuthLoading><LoadingLogo /></AuthLoading>
     </ConvexProviderWithClerk>
   </ClerkProvider>
 }
